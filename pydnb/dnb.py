5ebe87a48b44d9c62dd8621fb24c70c485ea3318
Tomas Sabata <speedex505@gmail.com>
2017-08-27 22:24:49 +0200

    add transition and emission probabilities

--- pydnb/dnb.py
+++ pydnb/dnb.py
@@ -11,6 +11,7 @@ class DNB:
     def __init__(self, debug=False):
         self.states_prior = None
         self.states_list = None
+        self.features_list = None
         self.A = None
         self.B = None
         self.debug = debug
@@ -45,12 +46,31 @@ class DNB:
     def _features_mle(self, df, state):
         """simplified_version"""
         import scipy.stats as st
-        features = list(df.columns.values)
-        for f in features:
+        self.features_list = list(df.columns.values)
+        for f in self.features_list:
             params = st.norm.fit(df[f])
             arg = params[:-2]
             loc = params[-2]
             scale = params[-1]
             if self.debug:
                 print("%s, %s, %s"%(str(arg),str(loc),str(scale)))
-            self.B[(state, f)] = params
+            self.B[(state, f)] = [st.norm] + params
+
+    def _emission_prob(self,state,data):
+        prob = 1
+        for f in self.features_list:
+            dist = self.B[(state,f)][0]
+            arg = self.B[(state,f)][1:-2]
+            loc = self.B[(state,f)][-2]
+            scale = self.B[(state,f)][-1]
+            prob *= dist.pdf(data.iloc[0][f], loc=loc, scale=scale, *arg)
+        return prob
+
+    def _transition_prob(self,state1, state2):
+        return self.A[np.searchsorted(self.states_list, state1), np.searchsorted(self.states_list, state2)]
+
+    def viterbi(self, data):
+        pass
+
+    def seq_probability(self, data, path):
+        pass
